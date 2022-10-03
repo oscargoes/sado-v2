8547e8b8d940b49af572607d3965d8e57556e727
SameerIslam <islamsameer91@gmail.com>
2022-10-03 12:56:50 -0500

    deploycommands

---
+++ deploy-commands.js
@@ -0,0 +1,14 @@
+const { REST, SlashCommandBuilder, Routes } = require('discord.js');
+const { clientId, guildId, token } = require('./config.json');
+
+const commands = [
+	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+	
+]
+	.map(command => command.toJSON());
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then((data) => console.log(`Successfully registered ${data.length} application commands.`))
+	.catch(console.error);
